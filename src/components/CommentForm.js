845c9a088c07958a7bf4a5e8b88a7d5796e9c990
Teddy mwangi <teddy.maina@student.moringaschool.com>
2023-11-22 10:36:28 +0300

    added components

---
+++ src/components/CommentForm.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const CommentForm = ({ addComment }) => {
+  const [comment, setComment] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (comment.trim() !== '') {
+      addComment(comment);
+      setComment('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <textarea
+        placeholder="Enter your comment"
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+      ></textarea>
+      <button type="submit">Comment</button>
+    </form>
+  );
+};
+
+export default CommentForm;
