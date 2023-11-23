bc873f12f913beef02764d763a223bc0fa260bbd
MartinWakaba <martin.wakaba@student.moringaschool.com>
2023-11-23 12:25:47 +0300

    Adding email

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,51 +2,44 @@ import React, { useState } from 'react';
 import CommentForm from '../components/CommentForm';
 import CompanyInfo from '../components/CompanyInfo';
 import Footer from '../components/Footer';
-import EmailForm from '../components/EmailForm';
+
+
 const Contact = () => {
   const [comments, setComments] = useState([]);
+  const [email, setEmail] = useState('');
 
   const addComment = (newComment) => {
     setComments([...comments, newComment]);
   };
-  const Contact = () => {
-    
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
   };
+
   return (
     <>
-    <div>
-      <div className="email-container">
-        <EmailForm />
-      </div>
-    </div>
       <div className="contact-container">
-      <div className="header">
-        <h1>Contact Form</h1>
+        <div className="header">
+          <h1>Contact Form</h1>
+        </div>
+        <div className="comments-section">
+          <h2>Contact Form</h2>
+          <CommentForm addComment={addComment} />
+          <ul className="comment-list">
+            {comments.map((comment, index) => (
+              <li key={index} className="comment-item">
+                {comment}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="company-info">
+          <CompanyInfo email="info@househunt.com" />
+        </div>
+        <hr className="divider" />
       </div>
-      <div className="comments-section">
-        <h2>Comments</h2>
-        <CommentForm addComment={addComment} />
-        <ul className="comment-list">
-          {comments.map((comment, index) => (
-            <li key={index} className="comment-item">
-              {comment}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="company-info">
-        <CompanyInfo email="info@househunt.com" />
-      </div>
-      <hr className="divider" />
-      
-    </div>
-  
-
-    <Footer />
+      <Footer />
     </>
-    
-
- 
   );
 };
 
